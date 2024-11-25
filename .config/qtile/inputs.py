1c72b87f7a69238def20f7a3b3a0fbbae6e96555
leugimkm <leugim911@hotmail.com>
2024-11-24 23:27:20 -0500

    update

--- .config/qtile/inputs.py
+++ .config/qtile/inputs.py
@@ -1,35 +1,37 @@
-from libqtile.config import Click, Drag, Group, Key
+from libqtile.config import EzClick as Click, EzDrag as Drag, EzKey as Key
+from libqtile.config import Group
 from libqtile.lazy import lazy
 
 from consts import ONE, TWO, THREE, FOUR, FIVE, SIX
 
-MOD, ALT, ATLGR = "mod4", "mod1", "mod5"
 TERMINAL = "kitty"
 
 keys = [
-    Key([MOD, ALT], "h", lazy.layout.left()),
-    Key([MOD, ALT], "l", lazy.layout.right()),
-    Key([MOD, ALT], "j", lazy.layout.down()),
-    Key([MOD, ALT], "k", lazy.layout.up()),
-    Key([MOD], "space", lazy.widget["keyboardlayout"].next_keyboard()),
-    Key([MOD, "shift"], "h", lazy.layout.shuffle_left()),
-    Key([MOD, "shift"], "l", lazy.layout.shuffle_right()),
-    Key([MOD, "shift"], "j", lazy.layout.shuffle_down()),
-    Key([MOD, "shift"], "k", lazy.layout.shuffle_up()),
-    Key([MOD, "control"], "h", lazy.layout.grow_left()),
-    Key([MOD, "control"], "l", lazy.layout.grow_right()),
-    Key([MOD, "control"], "j", lazy.layout.grow_down()),
-    Key([MOD, "control"], "k", lazy.layout.grow_up()),
-    Key([MOD], "n", lazy.layout.normalize()),
-    Key([MOD, "shift"], "Return", lazy.layout.toggle_split()),
-    Key([MOD], "Return", lazy.spawn(TERMINAL)),
-    Key([MOD], "Tab", lazy.next_layout()),
-    Key([MOD], "w", lazy.window.kill()),
-    Key([MOD], "f", lazy.window.toggle_fullscreen()),
-    Key([MOD], "t", lazy.window.toggle_floating()),
-    Key([MOD, "control"], "r", lazy.reload_config()),
-    Key([MOD, "control"], "q", lazy.shutdown()),
-    Key([MOD], "r", lazy.spawncmd()),
+    Key(("M-A-h"), lazy.layout.left()),
+    Key(("M-A-l"), lazy.layout.right()),
+    Key(("M-A-j"), lazy.layout.down()),
+    Key(("M-A-k"), lazy.layout.up()),
+    Key(("M-j"), lazy.group.next_window(), lazy.window.bring_to_front()),
+    Key(("M-k"), lazy.group.prev_window(), lazy.window.bring_to_front()),
+    Key(("M-<space>"), lazy.widget["keyboardlayout"].next_keyboard()),
+    Key(("M-S-h"), lazy.layout.shuffle_left()),
+    Key(("M-S-l"), lazy.layout.shuffle_right()),
+    Key(("M-S-j"), lazy.layout.shuffle_down()),
+    Key(("M-S-k"), lazy.layout.shuffle_up()),
+    Key(("M-C-h"), lazy.layout.grow_left()),
+    Key(("M-C-l"), lazy.layout.grow_right()),
+    Key(("M-C-j"), lazy.layout.grow_down()),
+    Key(("M-C-k"), lazy.layout.grow_up()),
+    Key(("M-n"), lazy.layout.normalize()),
+    Key(("M-S-<Return>"), lazy.layout.toggle_split()),
+    Key(("M-<Return>"), lazy.spawn(TERMINAL)),
+    Key(("M-<Tab>"), lazy.next_layout()),
+    Key(("M-w"), lazy.window.kill()),
+    Key(("M-f"), lazy.window.toggle_fullscreen()),
+    Key(("M-t"), lazy.window.toggle_floating()),
+    Key(("M-C-r"), lazy.reload_config()),
+    Key(("M-C-q"), lazy.shutdown()),
+    Key(("M-r"), lazy.spawncmd()),
 ]
 
 groups = [
@@ -47,21 +49,21 @@ groups = [
 for group in groups:
     keys.extend([
         Key(
-            [MOD], group.name,
+            f"M-{group.name}",
             lazy.group[group.name].toscreen(),
             desc="Switch to group {}".format(group.name),
         ),
         Key(
-            [MOD, "shift"], group.name,
+            f"M-S-{group.name}",
             lazy.window.togroup(group.name, switch_group=True),
             desc="Switch to & move focused W to group {}".format(group.name),
         ),
     ])
 
 mouse = [
-    Drag([MOD], "Button1", lazy.window.set_position_floating(),
+    Drag("M-1", lazy.window.set_position_floating(),
         start=lazy.window.get_position()),
-    Drag([MOD], "Button3", lazy.window.set_size_floating(),
+    Drag("M-3", lazy.window.set_size_floating(),
         start=lazy.window.get_size()),
-    Click([MOD], "Button2", lazy.window.bring_to_front()),
+    Click("M-2", lazy.window.bring_to_front()),
 ]
