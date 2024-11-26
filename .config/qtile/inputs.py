c93381d0dd4dc2856318f03d9384fa5738b7c48a
leugimkm <leugim911@hotmail.com>
2024-11-25 23:40:26 -0500

    update

--- .config/qtile/inputs.py
+++ .config/qtile/inputs.py
@@ -35,7 +35,7 @@ keys = [
     Key(("M-<Return>"), lazy.spawn(TERMINAL)),
     Key(("M-<Tab>"), lazy.next_layout()),
     Key(("M-w"), lazy.window.kill()),
-    Key(("M-f"), lazy.window.toggle_fullscreen()),
+    Key(("M-f"), lazy.window.toggle_fullscreen(), lazy.hide_show_bar()),
     Key(("M-t"), lazy.window.toggle_floating()),
     Key(("M-C-r"), lazy.reload_config()),
     Key(("M-C-q"), lazy.shutdown()),
