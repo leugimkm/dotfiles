2e3ce79f6fb855432f78cde89f4f2491e097a5b1
leugimkm <leugimkm@systrien.com>
2025-08-31 16:22:00 -0500

    u: update

--- .config/nvim/lsp/clangd.lua
+++
@@ -1,5 +0,0 @@
-return {
-  cmd = { "clangd", "--background-index" },
-  root_markers = { "compile-comands.json", "compile_flags.txt" },
-  filetypes = { "c", "cpp" },
-}
--- .config/nvim/lsp/lua_ls.lua
+++
@@ -1,5 +0,0 @@
-return {
-  cmd = { "lua-language-server" },
-  root_markers = { ".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "selene.toml", ".git" },
-  filetypes = { "lua" },
-}
